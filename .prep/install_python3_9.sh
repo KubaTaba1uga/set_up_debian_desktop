08ddc40492b17d874278e2d17bcb2b8c85329ead
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-28 22:42:38 +0100

    Update install_python3_9.sh

--- .prep/install_python3_9.sh
+++ .prep/install_python3_9.sh
@@ -4,6 +4,10 @@ sudo apt-get install wget build-essential libreadline-gplv2-dev  ca-certificates
 
 cd .prep
 
+mkdir python_installation
+
+cd python_installation
+
 wget https://github.com/actions/python-versions/releases/download/3.9.8-118810/python-3.9.8-linux-18.04-x64.tar.gz
 
 tar xzf python-3.9.8-linux-18.04-x64.tar.gz
