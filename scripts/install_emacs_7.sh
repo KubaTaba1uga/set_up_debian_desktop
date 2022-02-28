48a9b786cb072e216299ebc89090d84cee664f45
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-28 23:46:28 +0100

    Update install_emacs_7.sh

--- scripts/install_emacs_7.sh
+++ scripts/install_emacs_7.sh
@@ -2,24 +2,30 @@
 
 non_interactive="DEBIAN_FRONTEND=noninteractive"
 
-# Install elpy requirements
-sudo $non_interactive apt-get install virtualenv black flake8 pylint mypy -y
-python3 -m pip install jedi 
+if ! [ -x "$(command -v emacs)" ]; then
 
-# Install emacs
-sudo $non_interactive apt-get install -y build-essential git autoconf texinfo libgnutls28-dev libxml2-dev libncurses5-dev libjansson-dev bear
+  # Install elpy requirements
+  sudo $non_interactive apt-get install virtualenv black flake8 pylint mypy -y
+  python3 -m pip install jedi 
 
-mkdir /tmp/install_emacs
+  # Install emacs
+  sudo $non_interactive apt-get install -y build-essential git autoconf texinfo libgnutls28-dev libxml2-dev libncurses5-dev libjansson-dev bear
 
-git clone https://github.com/emacs-mirror/emacs.git /tmp/install_emacs
+  mkdir /tmp/install_emacs
 
-/tmp/install_emacs/emacs/autogen.sh CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" 
-/tmp/install_emacs/emacs/configure --with-modules --with-json
+  git clone https://github.com/emacs-mirror/emacs.git /tmp/install_emacs
 
-cd /tmp/install_emacs/emacs
+  /tmp/install_emacs/autogen.sh CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" 
+  /tmp/install_emacs/configure --with-modules --with-json
 
-bear make -j$(nproc)
+  cd /tmp/install_emacs/
 
-sudo make install
+  bear make -j$(nproc)
+
+  sudo make install
+
+
+
+fi
 
 # cp ./emacs/.emacs ~/
