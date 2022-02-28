b2d1002c9b6fb6a28fe83e221a07a2322d9137ff
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-28 23:31:20 +0100

    Update install_docker_4.sh

--- scripts/install_docker_4.sh
+++ scripts/install_docker_4.sh
@@ -2,35 +2,40 @@
 
 non_interactive="DEBIAN_FRONTEND=noninteractive"
 
-sudo apt-get remove -y \
- runc \
- docker \ 
- docker.io \
- containerd \
- docker-engine 
+if ! [ -x "$(command -v docker)" ]; then
 
-sudo $non_interactive apt-get  install -y \
+ sudo apt-get remove -y \
+  runc \
+  docker \ 
+  docker.io \
+  containerd \
+  docker-engine 
+
+ sudo $non_interactive apt-get  install -y \
     ca-certificates \
     curl \
     gnupg \
     lsb-release \
     apt-transport-https
 
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo echo \
+ sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+ sudo apt-get update
 
-sudo $non_interactive apt-get install -y \
- docker-ce \
- docker-ce-cli \
- containerd.io 
+ sudo $non_interactive apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io 
 
-# Allow forwarding packets
-sudo iptables -P FORWARD ACCEPT
+ # Allow forwarding packets
+ sudo iptables -P FORWARD ACCEPT
 
-# Save iptables
-sudo iptables-save | sudo tee /etc/iptables/rules.v4 > /dev/null
+ # Save iptables
+ sudo iptables-save | sudo tee /etc/iptables/rules.v4 > /dev/null
+
+fi
+
