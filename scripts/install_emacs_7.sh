093be5d53bdaff7874aa8a558b74bccd1d1f1f0a
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-28 23:24:14 +0100

    Update install_emacs_7.sh

--- scripts/install_emacs_7.sh
+++ scripts/install_emacs_7.sh
@@ -7,15 +7,19 @@ sudo $non_interactive apt-get install virtualenv black flake8 pylint mypy -y
 python3 -m pip install jedi 
 
 # Install emacs
-sudo apt-get install -y build-essential git autoconf texinfo libgnutls28-dev libxml2-dev libncurses5-dev libjansson-dev bear
+sudo $non_interactive apt-get install -y build-essential git autoconf texinfo libgnutls28-dev libxml2-dev libncurses5-dev libjansson-dev bear
 
-git clone git://git.sv.gnu.org/emacs.git /tmp/
+mkdir /tmp/install_emacs
 
-/tmp/emacs/autogen.sh CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" 
-/tmp/emacs/configure --with-modules --with-json
+git clone git://git.sv.gnu.org/emacs.git /tmp/install_emacs
+
+/tmp/install_emacs/emacs/autogen.sh CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" 
+/tmp/install_emacs/emacs/configure --with-modules --with-json
+
+cd /tmp/install_emacs/emacs
 
 bear make -j$(nproc)
 
 sudo make install
 
-cp ./emacs/.emacs ~/
+# cp ./emacs/.emacs ~/
