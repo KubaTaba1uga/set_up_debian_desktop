3a9278c6c435995807c55e72c991798b803d02f4
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-03-16 19:11:46 +0100

    Update install_iptables_1.sh

--- scripts/install_iptables_1.sh
+++ scripts/install_iptables_1.sh
@@ -13,20 +13,24 @@ sudo $non_interactive apt-get install iptables-persistent -y
 # Allow DNS 
 sudo iptables -I INPUT -p udp --sport 53 -j ACCEPT
 sudo iptables -I INPUT -p tcp --sport 53 -j ACCEPT
+
 # Allow HTTP
 ## For all output packages
 sudo iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 ## For input packages which are going back
 sudo iptables -A INPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+
 # Allow HTTPS
 ## For all output packages
 sudo iptables -A OUTPUT -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 ## For input packages which are going back
 sudo iptables -A INPUT -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
-## Allow from localhost
+
+# Allow localhost
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
-## Allow NTP
+
+# Allow NTP
 sudo iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 sudo iptables -A INPUT -p udp --sport 123 -j ACCEPT
 
